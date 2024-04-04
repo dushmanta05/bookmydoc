4538ebd71c0c7850c78ba52f74556feaac1bcc78
Dushmanta <dushmanta0511@gmail.com>
2024-04-04 14:31:54 +0530

    Implement user creation validation

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,12 +5,38 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const {
+  trimWhitespace,
+  checkNullValues,
+  checkRequiredFields,
+  isEmailValid,
+} = require("../utils/utils");
+
 module.exports = {
   create: async function (req, res) {
     try {
-      const userData = req.allParams();
-      const existingUser = await User.findOne({ email: userData.email });
+      const requiredFields = ["firstName", "lastName", "email", "password"];
+      const trimReqBody = trimWhitespace(req.body);
 
+      const nullValuesResponse = checkNullValues(trimReqBody);
+      if (!nullValuesResponse.status) {
+        return res.status(400).json(nullValuesResponse);
+      }
+
+      const requiredFieldsResponse = checkRequiredFields(
+        trimReqBody,
+        requiredFields
+      );
+      if (!requiredFieldsResponse.status) {
+        return res.status(400).json(requiredFieldsResponse);
+      }
+
+      const emailValidationResponse = isEmailValid(trimReqBody.email);
+      if (!emailValidationResponse.status) {
+        return res.status(400).json(emailValidationResponse);
+      }
+
+      const existingUser = await User.findOne({ email: trimReqBody.email });
       if (existingUser) {
         return res.status(400).json({
           status: false,
@@ -19,7 +45,7 @@ module.exports = {
         });
       }
 
-      const createdUser = await User.create(userData).fetch();
+      const createdUser = await User.create(trimReqBody).fetch();
       return res.status(201).json({
         status: true,
         message: "user created successfully",
