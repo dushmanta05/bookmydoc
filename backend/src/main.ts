5ba2ec2d365f1d6e0a2f62f2f793ca723604136c
Dushmanta <dushmanta0511@gmail.com>
2024-11-01 18:12:20 +0530

    chore: migrate application from express to fastify

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  await app.listen({ port: 3000 }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server is running on ${address}`);
+  });
+  console.log(`App URL: ${await app.getUrl()}`);
 }
 bootstrap();
